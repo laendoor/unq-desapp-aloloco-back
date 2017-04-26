428416b2a609acecb8da8c19f308422ea054bc68
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2017-04-26 13:36:36 +0000

    new try

--- tests/Integrations/IntegrationsTestCase.php
+++ tests/Integrations/IntegrationsTestCase.php
@@ -1,25 +1,10 @@
 <?php
 namespace Tests\Integrations;
 
-use Artisan;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 abstract class IntegrationsTestCase extends TestCase
 {
-//    use DatabaseMigrations;
-
-    protected function setUp()
-    {
-        parent::setUp();
-
-        Artisan::call('migrate');
-    }
-
-    protected function tearDown()
-    {
-        Artisan::call('migrate:rollback');
-
-        parent::tearDown();
-    }
+    use DatabaseMigrations;
 }
