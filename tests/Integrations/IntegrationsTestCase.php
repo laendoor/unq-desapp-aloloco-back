282c2ffb394dd0a73b368d0a172c89905d1dbd10
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2017-04-26 13:10:14 +0000

    trying running migration explicity

--- tests/Integrations/IntegrationsTestCase.php
+++ tests/Integrations/IntegrationsTestCase.php
@@ -1,10 +1,25 @@
 <?php
 namespace Tests\Integrations;
 
+use Artisan;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
 abstract class IntegrationsTestCase extends TestCase
 {
-    use DatabaseMigrations;
+//    use DatabaseMigrations;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        Artisan::call('migrate');
+    }
+
+    protected function tearDown()
+    {
+        Artisan::call('migrate:rollback');
+
+        parent::tearDown();
+    }
 }
