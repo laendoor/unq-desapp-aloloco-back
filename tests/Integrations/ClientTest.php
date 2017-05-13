0f9d9d96ee7b3c390b891ebd63175217ceff81f1
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2017-05-13 21:29:32 +0000

    remove trtash

--- tests/Integrations/ClientTest.php
+++ tests/Integrations/ClientTest.php
@@ -62,30 +62,4 @@ class ClientTest extends IntegrationsTestCase
         $this->assertEquals(1, $dany_db->getShoppingLists()->count());
         $this->assertEquals('Kingdoms to Conquer', $dany_db->getShoppingLists()->first()->getName());
     }
-
-
-//    /**
-//     * @test
-//     *
-//     * @return void
-//     */
-//    public function it_stores_and_reads_a_model_from_the_database_using_factories(): void
-//    {
-//        // Arrange
-//        $repository = EntityManager::getRepository(Product::class);
-//        entity(Product::class)->create([
-//            'name'  => 'Papas Fritas',
-//            'brand' => 'Lays',
-//            'image' => 'lays.jpg'
-//        ]);
-//
-//        // Act
-//        $product = $repository->findOneByBrand('Lays');
-//
-//        // Assert
-//        $this->assertEquals(1, count($repository->findAll()));
-//        $this->assertEquals('Papas Fritas', $product->getName());
-//        $this->assertEquals('Lays', $product->getBrand());
-//        $this->assertEquals('lays.jpg', $product->getImage());
-//    }
 }
